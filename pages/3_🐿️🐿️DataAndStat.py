c088c37327de4fc0c61f9aa8c87708b7ff844989
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-19 16:50:54 +0700

    .

--- pages/3_🐿️🐿️DataAndStat.py
+++ pages/3_🐿️🐿️DataAndStat.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 st.header("Show Data Index Price")
 
-df=pd.read_csv("./data/stock_index_price.csv")
+df=pd.read_csv("./data/car_pri.csv")
 st.write(df.head(10))
 
 st.header("Show Chart")
 
 st.line_chart(
-   df, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
+   df, x="car_pri", y=["year", "make"], color=["#FF0000", "#0000FF"]  # Optional
 )
