e4e344cba5eef3d8d6db6ac014bbe02a6c8cebd1
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-19 17:34:49 +0700

    Update 3_📈DataAndStat.py

--- pages/3_📈DataAndStat.py
+++ pages/3_📈DataAndStat.py
@@ -3,11 +3,13 @@ import pandas as pd
 
 st.header("Show Data Index Price")
 
-df=pd.read_csv("./data/car_pri.csv")
-st.write(df.head(10))
+df = pd.read_csv("./data/car_pri.csv")
+
+st.subheader("Columns in DataFrame:")
+st.write(df.columns)
 
 st.header("Show Chart")
 
 st.line_chart(
-    df[['car_pri', 'sellingprice', 'unemployment_rate']]
+    df[['car_pri', 'make', 'sellingprice']]
 )
