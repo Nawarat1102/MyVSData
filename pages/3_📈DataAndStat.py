adadfbf2bd2c084b0d34cdbbf1903d1b8b3487ee
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-20 10:37:11 +0700

    Update 3_📈DataAndStat.py

--- pages/3_📈DataAndStat.py
+++ pages/3_📈DataAndStat.py
@@ -6,8 +6,8 @@ st.header("Show Data Index Price")
 df=pd.read_csv("./data/car_pri.csv")
 st.write(df.head(10))
 
-st.header("Show Chart")
-statistics = df.describe()
+#st.header("Show Chart")
+#statistics = df.describe()
 
 st.write("ค่าสถิติของข้อมูล:")
 st.write(statistics)
