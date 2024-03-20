9d93c69fa92ed4a2ed636e41999070f04277d4b6
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-20 11:38:49 +0700

    Update 4_ 📉 Visualization.py

--- pages/4_ 📉 Visualization.py
+++ pages/4_ 📉 Visualization.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("/path/to/car_pri.csv")
+df=pd.read_csv("./data/car_pri.csv")
 
 
-plt.scatter(df["make"], df["model"])
+plt.scatter(df['make'], df['model'])
 plt.xlabel("Make")
 plt.ylabel("Model")
 plt.show()
