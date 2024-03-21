a7809e61f6cc8446b5680b6eeec3d6ef8e8849c4
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-21 11:54:39 +0700

    Update 4_ 📉 Visualization.py

--- pages/4_ 📉 Visualization.py
+++ pages/4_ 📉 Visualization.py
@@ -1,7 +1,16 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df=pd.read_csv("./data/app1.csv")
+df = pd.read_csv("./data/app1.csv")
 
 st.bar_chart(df, x='Sex', y='BMI')
 
+BMI = df['BMI']
+Severity = df['Severity']
+
+plt.scatter(BMI, Severity)
+plt.xlabel('BMI')
+plt.ylabel('Severity')
+plt.title('กราฟกระจายแสดงความสัมพันธ์ระหว่าง BMI กับความรุนแรง')
+plt.show()
