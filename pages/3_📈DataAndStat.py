814ed04b8cc4b3a90ee71baa5f7433894f9cc69e
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-21 20:07:41 +0700

    Update 3_📈DataAndStat.py

--- pages/3_📈DataAndStat.py
+++ pages/3_📈DataAndStat.py
@@ -5,27 +5,27 @@ import streamlit as st
 df = pd.read_csv("./data/app1.csv")
 
 
-# สร้างแผนภูมิแท่ง
+
 fig_bar = px.bar(df['Sex'].value_counts(), title='กราฟแผนภูมิแท่ง : อัตราการเกิดในเพศ')
 st.plotly_chart(fig_bar)
 
 fig_box_bmi = px.box(df, y="Age", title="Box plot : อัตราการเกิดในช่วงอายุ")
 st.plotly_chart(fig_box_bmi)
 
-# สร้างกราฟแบบแท่ง
+
 fig_box_bmi = px.box(df, y="BMI", title="Box plot : ดัชนีมวลกาย")
 st.plotly_chart(fig_box_bmi)
 
-# สร้างกราฟแบบแท่ง
-fig_box_bmi = px.box(df, y="Length_of_Stay", title="Box plot : ดัชนีมวลกาย")
+
+fig_box_bmi = px.box(df, y="Length_of_Stay", title="Box plot : ระยะเวลาในการพักฟื้น")
 st.plotly_chart(fig_box_bmi)
 
-# สร้างกราฟวงกลม
+
 management_counts = df['Management'].value_counts()
 fig_pie = px.pie(values=management_counts.values, names=management_counts.index, title='Plotly Express : อัตราการรักษาในแต่ละแบบ')
 st.plotly_chart(fig_pie)
 
-# สร้างกราฟวงกลม
+
 management_counts = df['Severity'].value_counts()
 fig_pie = px.pie(values=management_counts.values, names=management_counts.index, title='Plotly Express : อัตราความรุนแรงของการเกกิด')
 st.plotly_chart(fig_pie)
