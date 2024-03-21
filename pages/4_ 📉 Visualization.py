f6646b037f14ba487c1416552c9681da441cdd36
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-21 20:45:18 +0700

    Update 4_ 📉 Visualization.py

--- pages/4_ 📉 Visualization.py
+++ pages/4_ 📉 Visualization.py
@@ -34,27 +34,3 @@ st.plotly_chart(fig_bar)
 
 
 
-
-# สร้างแผนภูมิเส้น
-fig_line = px.line(df, x="Weight", y="Sex")
-
-# สร้างแผนภูมิแท่ง
-fig_bar = px.bar(df, x="Age", y="BMI")
-
-# สร้างแผนภูมิเปรียบเทียบ
-#fig_box = px.box(df, x='Sex', y='BMI')
-
-# สร้างแผนภูมิแผ่น
-fig_pie = px.pie(df, names='Length_of_Stay')
-
-# แสดงแผนภูมิโดยใช้คำสั่ง st.plotly_chart()
-st.plotly_chart(fig_pie)
-
-# แสดงแผนภูมิโดยใช้คำสั่ง st.plotly_chart()
-st.plotly_chart(fig_box)
-
-# แสดงกราฟโดยใช้คำสั่ง st.plotly_chart()
-st.plotly_chart(fig_line)
-st.plotly_chart(fig_bar)
-
-st.image("./img/ti.jpg")
