599275d19591ff462d5104e4f2b2bbaff54a1b25
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-21 21:34:29 +0700

    Update 4_ 📉 Visualization.py

--- pages/4_ 📉 Visualization.py
+++ pages/4_ 📉 Visualization.py
@@ -8,24 +8,26 @@ st.image("./img/ti.jpg")
 # เช่น
 df = pd.read_csv("./data/app1.csv")
 
-
-fig_scatter = px.scatter(df, x='Age', y='BMI', title='Scatter Plot: การเปรียบเทียบการเกิดระหว่างดัชนีมวลกายและอายุ')
+st.header("การเปรียบเทียบการเกิดระหว่างดัชนีมวลกายและอายุ")
+fig_scatter = px.scatter(df, x='Age', y='BMI', title='Scatter Plot : ดัชนีมวลกายและอายุ')
 st.plotly_chart(fig_scatter)
 
 
+st.header("จากกราฟด้านบนนำมาแสดงการเปรียบเทียบความรุนของการเกิดเพิ่มเติมได้")
 fig_scatter = px.scatter(df, x="Age", y="BMI", color="Severity", hover_data=["Sex", "Height", "Weight", "Length_of_Stay"],
-                title="Scatter Plot: จากกราฟด้านบนนำมาแสดงการเปรียบเทียบความรุนของการเกิดเพิ่มเติมได้")
+                title="Scatter Plot : แสดงการเปรียบเทียบความรุนของการเกิด")
 st.plotly_chart(fig_scatter)
 
 
+st.header("เปรียบเทียบดัชนีมวลกายและการรักษาระหว่างเพศ")
 fig_bar = px.bar(df, x="Management", y="BMI", color="Sex", barmode="group",
-                 title="Bar Plot: เปรียบเทียบดัชนีมวลกายและความรุนแกรงระหว่างเพศ")
+                 title="Bar Plot: การรักษาแบบอนุรัษณ์นิยม ")
 st.plotly_chart(fig_bar)
 
 
-
+st.header("เปรียบเทียบดัชนีมวลกายและความรุนแกรงระหว่างเพศ")
 fig_bar = px.bar(df, x="Severity", y="BMI", color="Sex", barmode="group",
-                 title="Bar Plot: เปรียบเทียบดัชนีมวลกายและความรุนแกรงระหว่างเพศ")
+                 title="Bar Plot: ความรุนแรง อักเสบ รุนแรง")
 st.plotly_chart(fig_bar)
 
 
