75e773af5e452724d033ac8ace88219d2ace564f
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-21 17:32:12 +0700

    Update 3_📈DataAndStat.py

--- pages/3_📈DataAndStat.py
+++ pages/3_📈DataAndStat.py
@@ -7,9 +7,7 @@ df = pd.read_csv("./data/app1.csv")
 
 
 # สร้างแผนภูมิแท่ง
-fig_bar = px.bar(df['Sex'].value_counts(), title='Gender Comparison',
-                 color=df['Sex'].value_counts().index.map({'Male': 'royalblue', 'Female': 'red'}),
-                 color_discrete_map={'Male': 'royalblue', 'Female': 'red'})
+fig_bar = px.bar(df['Sex'].value_counts(), title='Gender Comparison')
 st.plotly_chart(fig_bar)
 
 
