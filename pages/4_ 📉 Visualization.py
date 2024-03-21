1091d33e1f362475b7251dbaf8ce1676f4148333
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-21 10:24:19 +0700

    Update 4_ 📉 Visualization.py

--- pages/4_ 📉 Visualization.py
+++ pages/4_ 📉 Visualization.py
@@ -5,3 +5,8 @@ df=pd.read_csv("./data/app1.csv")
 
 st.bar_chart(df, x='Sex', y='BMI')
 
+plt.scatter(BMI, Severity)
+plt.xlabel('BMI')
+plt.ylabel('Severity')
+plt.title('กราฟกระจายแสดงความสัมพันธ์ระหว่าง BMI กับความรุนแรง')
+plt.show()
