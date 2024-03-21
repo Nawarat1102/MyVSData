0d3155f79f3559346418d84807cb7104750fd440
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-21 12:47:03 +0700

    Update 4_ 📉 Visualization.py

--- pages/4_ 📉 Visualization.py
+++ pages/4_ 📉 Visualization.py
@@ -14,6 +14,12 @@ fig_line = px.line(df, x="Sex", y="Weight")
 # สร้างแผนภูมิแท่ง
 fig_bar = px.bar(df, x="Age", y="BMI")
 
+# สร้างแผนภูมิเปรียบเทียบ
+fig_box = px.box(df, x='Sex', y='BMI')
+
+# แสดงแผนภูมิโดยใช้คำสั่ง st.plotly_chart()
+st.plotly_chart(fig_box)
+
 # แสดงกราฟโดยใช้คำสั่ง st.plotly_chart()
 st.plotly_chart(fig_line)
 st.plotly_chart(fig_bar)
