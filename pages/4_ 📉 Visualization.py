a164dd1c7efa77efee35c6c62612672e7405234e
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-21 19:29:49 +0700

    Update 4_ 📉 Visualization.py

--- pages/4_ 📉 Visualization.py
+++ pages/4_ 📉 Visualization.py
@@ -12,7 +12,7 @@ fig_scatter = px.scatter(df, x="Age", y="BMI", color="Severity", hover_data=["Se
                 title="Scatter Plot: Age vs BMI")
 st.plotly_chart(fig_scatter)
 
-fig_line_height = px.line(df, x="Height",y="Weight", title="Height Growth")
+fig_line_height = px.line(df, x="Weight",y="Height", title="Height Growth")
 st.plotly_chart(fig_line_height)
 
 
