116c9b9d9f461080bb50d6936ae07a112aa81356
Nawarat1102 <156872600+Nawarat1102@users.noreply.github.com>
2024-03-21 12:50:50 +0700

    Update 4_ 📉 Visualization.py

--- pages/4_ 📉 Visualization.py
+++ pages/4_ 📉 Visualization.py
@@ -16,6 +16,11 @@ fig_bar = px.bar(df, x="Age", y="BMI")
 
 # สร้างแผนภูมิเปรียบเทียบ
 fig_box = px.box(df, x='Sex', y='BMI')
+# สร้างแผนภูมิแผ่น
+fig_pie = px.pie(df, names='Sex')
+
+# แสดงแผนภูมิโดยใช้คำสั่ง st.plotly_chart()
+st.plotly_chart(fig_pie)
 
 # แสดงแผนภูมิโดยใช้คำสั่ง st.plotly_chart()
 st.plotly_chart(fig_box)
